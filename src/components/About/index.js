8de9a1de47a5cc185ca5777483e6919c83232c2a
Robert Petersen <robert.petersen808@gmail.com>
2021-08-16 11:46:22 -0500

    Started about section / changed color and styles for nav and landing

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,17 +1,44 @@
-import styled from "styled-components";
+import React from "react";
+import { AboutContainer, AboutLeftContainer, AboutRightContainer, AboutLanguages, AboutTools, Icon, IconTitle, AboutSkillContainer, AboutSkill } from "./AboutElements";
+import Robert from "../../images/me.jpg";
+import { languagesData, toolsData } from "./data";
 
 function About() {
   return (
-    <div className="About">
-      
-
-    </div>
+    <AboutContainer id="about">
+      <AboutLeftContainer>
+        <img className="my-photo" src={Robert} alt="Robert Petersen"/>
+        <h2>Hello, I am Robert Petersen.</h2>
+        <p>
+          I am a full-stack web developer who loves problemand building web applications from the ground up. not coding I enjoy playing video games, watching Star Wars, fishing, hiking, camping, and walkingdog. I come from a retail background and have scustomer service, sales, lower management, management.
+        </p>
+      </AboutLeftContainer>
+      <AboutRightContainer>
+        <AboutLanguages>          
+          <h1>Languages and Frameworks</h1>
+          <AboutSkillContainer>
+            {languagesData.map((data)=>(
+              <AboutSkill>
+                <Icon src={data.src} alt={data.alt} />
+                <IconTitle>{data.title}</IconTitle>
+              </AboutSkill>
+            ))}
+          </AboutSkillContainer>
+        </AboutLanguages>
+        <AboutTools>
+          <h1>Tools</h1>
+          <AboutSkillContainer>
+            {toolsData.map((data)=>(
+                <AboutSkill>
+                  <Icon src={data.src} alt={data.alt} />
+                  <IconTitle>{data.title}</IconTitle>
+                </AboutSkill>
+            ))}
+          </AboutSkillContainer>
+        </AboutTools>
+      </AboutRightContainer>
+    </AboutContainer>
   );
 }
 
 export default About;
-
-const Background = styled.div`
-  width: 100%;
-  background-color: #262626;
-`
